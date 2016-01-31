83d05688ff90d0589a86d0b6b58c94eb3ff98ff5
Andrew Shen <andrewdshen@gmail.com>
2016-01-31 16:59:36 -0600

    added Content and Author schemas, added assignment 3 controller

--- models/Author.js
+++ models/Author.js
@@ -1,13 +1,13 @@
 var mongoose = require('mongoose'),
     Schema   = mongoose.Schema;
 
-var ContentSchema = new Schema({
-  title: { type: String, required: true, unique: true },
+var AuthorSchema = new Schema({
+  name: { type: String, required: true, unique: true },
   email: { type: String, required: true, required: true, unique: true },
   url: { type: String,required: true },
   type: { type: String },
-  author: { Object: { name: }}
+  work: [String],
   admin: { type: String }
 });
 
-module.exports = mongoose.model('contents', ContentSchema);
+module.exports = mongoose.model('authors', AuthorSchema);
--- models/User.js
+++ models/User.js
@@ -2,8 +2,8 @@ var mongoose = require('mongoose'),
     Schema   = mongoose.Schema;
 
 var UserSchema = new Schema({
-  name: { type: String, required: true},
-  email: { type: String, required: true, required: true, unique: true},
+  name: { type: String, required: true },
+  email: { type: String, required: true, required: true, unique: true },
   password: { type: String,required: true },
   gender: { type: String },
   admin: { type: String }
